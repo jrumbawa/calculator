2b3f2806b252d2976a2e784c5aa62c46f002909b
Jomar Rumbawa <rumbawajomar@gmail.com>
2023-11-28 16:22:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let firstNum = null
+let secondNum = null
+let operator = null
+
 function add(a, b) {
     return a + b
 }
@@ -12,4 +16,21 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b
+}
+
+function operate(operand, firstNum, secondNum) {
+    switch (operand) {
+        case '+':
+            add(firstNum, secondNum)
+            break;
+        case '-':
+            subtract(firstNum, secondNum)
+            break;
+        case '*':
+            multiply(firstNum, secondNum)
+            break;
+        case '/':
+            divide(firstNum, secondNum)
+            break;
+    }
 }
