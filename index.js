1d18c669ef893b873f8217995bd7dbf4626e1954
jrumbawa <rumbawajomar@gmail.com>
2023-12-04 17:43:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,90 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator = null;
+
 const display = document.getElementById("display")
-const numBtns = document.querySelectorAll(".num-btn")
-const operandBtns = document.querySelectorAll(".operand-btn")
-let firstNum = '0'
-let secondNum = null
-let operator = null
-let displayText = ''
 
-numBtns.forEach(numBtn => {
-    numBtn.addEventListener("click", function() {
-        handleNumberClick(numBtn.textContent)
-    })
-})
-
-operandBtns.forEach(operandBtn => {
-    operandBtn.addEventListener("click", function() {
-        console.log(operandBtn.textContent)
-    })
-})
-
-function handleNumberClick(num) {
-    if (firstNum === '0') {
-        firstNum = num
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
     } else {
-        firstNum += num
+        handleNumber(value);
     }
-    console.log(firstNum)
+    display.innerText = buffer
 }
 
-function add(a, b) {
-    return a + b
-}
-
-function subtract(a, b) {
-    return a - b
-}
-
-function multiply(a, b) {
-    return a * b
-}
-
-function divide(a, b) {
-    return a / b
-}
-
-function operate(operand, firstNum, secondNum) {
-    switch (operand) {
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'c':
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return
+            }
+            flushOperation(parseInt(buffer))
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
         case '+':
-            add(firstNum, secondNum)
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol)
+            break;   
+    }
+}
+
+function handleMath(symbol) {
+    if (buffer === "0") {
+        return;
+    }
+    
+    const intBuffer = parseInt(buffer)
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer
+    } else {
+        flushOperation(intBuffer)
+    }
+
+    previousOperator = symbol
+
+    buffer = "0"
+}
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString
+    } else {
+        buffer += numberString
+    }
+}
+
+function flushOperation(intBuffer) {
+    switch (previousOperator) {
+        case '+':
+            runningTotal += intBuffer;
             break;
         case '-':
-            subtract(firstNum, secondNum)
+            runningTotal -= intBuffer;
             break;
-        case '*':
-            multiply(firstNum, secondNum)
+        case '×':
+            runningTotal *= intBuffer;
             break;
-        case '/':
-            divide(firstNum, secondNum)
+        case '÷':
+            runningTotal /= intBuffer;
             break;
     }
 }
+
+function init() {
+    document.querySelector('.calc-buttons')
+        .addEventListener("click", function(event) {
+            buttonClick(event.target.innerText)
+        })
+}
+
+init()
