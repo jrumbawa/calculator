bbe5ddee445c050a8517bff090e4f58c6ca67067
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-02-07 19:01:47 -0800

    Create variables and math functions

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let numbOne = undefined;
+let numbTwo = undefined;
+let operator = undefined;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(numOne, numTwo, operator) {
+  switch (operator) {
+    case "+":
+      return add(numOne, numTwo);
+    case "-":
+      return subtract(numOne, numTwo);
+    case "*":
+      return multiply(numOne, numTwo);
+    case "/":
+      return divide(numOne, numTwo);
+  }
+}
